feb8e90bdb5476cdb4d8f2854a189343e72afca2
Your Name <you@example.com>
2018-01-29 19:34:31 +0800

    dream

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'assets/style/reset.css'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
